bc3169c44821337c06dd3fc8b13065ce30c218dc
Shabaz Sultan <shabaz.sultan@gmail.com>
2014-09-26 13:37:03 +0200

    basic outline for C++ app with SDL2 and OpenGL

---
+++ code/cppsimulator/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include "SDL.h"
+
+SDL_Window* initSDLAndOpenGL(int width, int height, bool vsync, bool fullscreen)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 32 );
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS , 1 );
+    SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES , 4 );
+    SDL_Window* screen;
+    if (fullscreen)
+    {
+        screen = SDL_CreateWindow("AHN2 Viewer",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                width, height,
+                SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
+    }
+    else
+    {
+        screen = SDL_CreateWindow("AHN2 Viewer",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                width, height,
+                SDL_WINDOW_OPENGL);
+    }
+
+    SDL_GLContext glcontext = SDL_GL_CreateContext(screen);
+
+    if (SDL_GL_SetSwapInterval(vsync? 1 : 0) == -1) {
+        std::cout << "could not set desired vsync mode: " << vsync << 
+            std::endl;
+    }
+    glClearColor(0.1, 0.1, 0.15, 0);
+
+    glClearDepth(1);
+    glDepthFunc(GL_LESS);
+
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(90, float(width)/height, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        std::cout << "ERROR after opengl initialization" << std::endl;
+        exit(1);
+    }
+    else
+    {
+        std::cout << "NO error after opengl initialization" << std::endl;
+    }
+
+    
+    return screen;
+}
+
+
+int main(int argc, char* argv[])
+{
+    int screenWidth = 800;
+    int screenHeight = 600;
+    bool vsync = true;
+    bool fullscreen = false;
+
+    SDL_Window* surface = initSDLAndOpenGL(screenWidth, screenHeight, vsync,
+            fullscreen);
+    bool running = true;
+    while (running)
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        SDL_GL_SwapWindow(surface);
+        SDL_Event event;    
+        while (SDL_PollEvent(&event)) 
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+    }
+}
